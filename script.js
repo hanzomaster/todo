276b7e0f8f4bac24edb57d2058526fedbfe52886
Thinh Tran <thinhrom11@gmail.com>
2022-05-14 11:23:53 +0700

    feat: change data type to JSON and live update data

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
+let data = [
+  {
+    title: 'Create a to do list',
+    completed: true,
+  },
+  {
+    title: 'Take out the trash',
+    completed: false,
+  },
+  {
+    title: 'Learn something new',
+    completed: false,
+  },
+  {
+    title: 'Feed my dog',
+    completed: false,
+  },
+  {
+    title: 'Homework',
+    completed: false,
+  },
+  {
+    title: 'Remind someone about something',
+    completed: false,
+  },
+]
+localStorage.setItem('todo', JSON.stringify(data))
+
 const addButton = document.getElementsByClassName('button')[0]
 const inputs = document.getElementsByTagName('input')
 const input = document.getElementById('new-item')
 const itemsList = document.getElementsByClassName('items')
+const checkboxes = document.querySelectorAll('input[type=checkbox]')
 
 addButton.addEventListener('click', () => {
   if (input.value !== '') {
@@ -16,10 +45,21 @@ addButton.addEventListener('click', () => {
     itemsList[0].appendChild(newItem)
     itemsList[0].appendChild(newLabel)
 
-    input.value = ''
-
     const updatedTodo = JSON.parse(localStorage.getItem('todo')) || []
-    updatedTodo.push(input.value)
+    updatedTodo.push({ title: input.value, completed: false })
     localStorage.setItem('todo', JSON.stringify(updatedTodo))
+
+    input.value = ''
   }
+})
+
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('click', () => {
+    const updatedTodo = JSON.parse(localStorage.getItem('todo')) || []
+    const index = updatedTodo.findIndex(
+      item => item.title === checkbox.nextElementSibling.innerText
+    )
+    updatedTodo[index].completed = checkbox.checked
+    localStorage.setItem('todo', JSON.stringify(updatedTodo))
+  })
 })
